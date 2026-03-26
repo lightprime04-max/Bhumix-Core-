56eae0f3329d12e3c774b1d93315d42bd9b60c1c
lightprime04-max <lightprime04@gmail.com>
2026-03-26 17:04:03 +0530

    Implement navigation functions in script.js
    Added multiple navigation functions for channel, shop, start, and login alerts.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function goToChannel(){
   window.open("https://youtube.com/@bhumixcore", "_blank");
 }
+<script>
+function goToChannel(){
+  window.open("https://youtube.com/@bhumixcore", "_blank");
+}
+
+function goToShop(){
+  alert("Shop page coming soon 🚀");
+}
+
+function goToStart(){
+  alert("Getting started... 🚀");
+}
+
+function goToLogin(){
+  alert("Login system coming soon 🔐");
+}
+</script>
